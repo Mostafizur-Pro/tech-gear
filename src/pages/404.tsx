a38c3e6a04d6a0c8ef498a401bdd10cf45e67fc5
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-12-27 00:32:12 +0600

    update

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+
+const ErrorPage = () => (
+ 
+      <section className="error-page text-center">
+        <div className="container">
+          <h1>Error 404</h1>
+          <p>Woops. Looks like this page doesn't exist</p>
+          <a href="/" className="btn btn--rounded btn--yellow">Go to home</a>
+        </div>
+      </section>
+   
+  )
+  
+  export default ErrorPage
