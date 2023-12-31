66b7630e16b53cf8f9e3a108081951e0e34719bf
SabbirSami <smd71430@gmail.com>
2023-12-31 12:13:59 +0600

    show product image in slider

--- src/pages/product-details/index.tsx
+++ src/pages/product-details/index.tsx
@@ -1,7 +1,16 @@
 import Link from "next/link";
+import { useState } from "react";
 import { RxSlash } from "react-icons/rx";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
 
 const ProductDetails = () => {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
     return (
         <section className="">
             <div className="py-10 bg-[#F5F5F5]">
@@ -39,7 +48,74 @@ const ProductDetails = () => {
                     <div className="col-span-3 p-6 bg-[#F8F8F8]">
                         <h2 className="">SideBar</h2>
                     </div>
-                    <div className="col-span-9 bg-[#F8F8F8]"></div>
+                    <div className="col-span-9 bg-[#F8F8F8]">
+                        <div className="grid grid-cols-7">
+                            <div className="col-span-3">
+                                <Swiper
+                                    spaceBetween={10}
+                                    navigation={true}
+                                    thumbs={{ swiper: thumbsSwiper }}
+                                    modules={[FreeMode, Navigation, Thumbs]}
+                                    className="mySwiper2"
+                                >
+                                    <SwiperSlide>
+                                        <img
+                                            className="h-[32rem] w-full object-cover"
+                                            src="https://im.uniqlo.com/global-cms/spa/res02e261446946bdac6265a2dccfbd4350fr.jpg"
+                                        />
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <img
+                                            className="h-[32rem] w-full object-cover"
+                                            src="https://cms.brnstc.de/product_images/680x930_retina/cpro/media/images/product/23/8/100149234813000_2_1692600358791.jpg"
+                                        />
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <img
+                                            className="h-[32rem]  w-full object-cover"
+                                            src="https://www.vbcpontedera.com/wp-content/uploads/2023/01/lanvin-t-shirt-avec-poche-noir-t-shirts-polos-homme_1-1.jpg"
+                                        />
+                                    </SwiperSlide>
+                                </Swiper>
+                                <div className="pt-2">
+                                    <Swiper
+                                        spaceBetween={10}
+                                        slidesPerView={4}
+                                        freeMode={true}
+                                        watchSlidesProgress={true}
+                                        modules={[FreeMode, Navigation, Thumbs]}
+                                        className="mySwiper"
+                                    >
+                                        <SwiperSlide>
+                                            <img
+                                                className="h-32 w-full object-cover"
+                                                src="https://im.uniqlo.com/global-cms/spa/res02e261446946bdac6265a2dccfbd4350fr.jpg"
+                                            />
+                                        </SwiperSlide>
+                                        <SwiperSlide>
+                                            <img
+                                                className="h-32 w-full object-cover"
+                                                src="https://cms.brnstc.de/product_images/680x930_retina/cpro/media/images/product/23/8/100149234813000_2_1692600358791.jpg"
+                                            />
+                                        </SwiperSlide>
+                                        <SwiperSlide>
+                                            <img
+                                                className="h-32 w-full object-cover"
+                                                src="https://www.vbcpontedera.com/wp-content/uploads/2023/01/lanvin-t-shirt-avec-poche-noir-t-shirts-polos-homme_1-1.jpg"
+                                            />
+                                        </SwiperSlide>
+                                        <SwiperSlide>
+                                            <img
+                                                className="h-32 w-full object-cover"
+                                                src="https://im.uniqlo.com/global-cms/spa/res02e261446946bdac6265a2dccfbd4350fr.jpg"
+                                            />
+                                        </SwiperSlide>
+                                    </Swiper>
+                                </div>
+                            </div>
+                            <div className="col-span-4"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
