837377c41a1914c1d4e55f4042f4409906fbbb10
SabbirSami <smd71430@gmail.com>
2023-12-31 11:18:20 +0600

    fix footer

---
+++ src/pages/product-details/index.tsx
@@ -0,0 +1,20 @@
+const ProductDetails = () => {
+    return (
+        <section className="container mx-auto px-">
+            <div className="py-10 bg-[#F5F5F5]">
+                <div className="">
+                    {" "}
+                    <h2 className="uppercase font-bold text-2xl">
+                        BLACK T-SHIRT
+                    </h2>
+                    <p className="text-xl font-medium text-gray-500">
+                        Le Manifique
+                    </p>
+                </div>
+            </div>
+            <h2 className="">Product Details</h2>
+        </section>
+    );
+};
+
+export default ProductDetails;
