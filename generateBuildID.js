cfb009e66c1c6fecea5980ffae74f28e866aa83b
mostafizur-pro <mostafizur0195@gmail.com>
2023-12-31 16:19:44 +0600

    .next file add

--- generateBuildID.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-// Generate a unique ID (example: current timestamp)
-const buildID = Date.now().toString();
-
-// Write the ID to the BUILD_ID file
-fs.writeFileSync('D:\\_mostafizur\\Diginie It\\tech-gear\\.next\\BUILD_ID', buildID);
-
-console.log('BUILD_ID created:', buildID);
